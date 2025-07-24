2f22ef53fcfd6a4db074d4683b7091b4e53a2154
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2025-07-24 12:40:18 +0100

    feat(routes): add contact us route to main router for handling inquiries

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,11 +8,13 @@ import express from 'express';
 import authRoutes from './auth/authRoute.js';
 import propertyRoutes from './propertyRoute.js';
 import newsLetterRoutes from './newsLetterRoute.js';
+import contactUsRoute from './contactUsRoute.js';
 
 const router = express.Router();
 
 router.use('/auth', authRoutes);
 router.use('/public', propertyRoutes);
 router.use('/', newsLetterRoutes);
+router.use('/', contactUsRoute);
 
 export default router;
